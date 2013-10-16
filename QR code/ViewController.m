af0f4c26529da56f7175e5ce86514cf59e6157f8
bin <r258833095@163.com>
2013-10-16 11:32:15 +0800

    new

--- QR code/ViewController.m
+++ QR code/ViewController.m
@@ -7,12 +7,17 @@
 //
 
 #import "ViewController.h"
+#import "ZBarSDK.h"
+#import "QRCodeGenerator.h"
 
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+@synthesize imageview;
+@synthesize text;
+@synthesize label;
 
 - (void)viewDidLoad
 {
@@ -22,6 +27,9 @@
 
 - (void)viewDidUnload
 {
+    [self setLabel:nil];
+    [self setImageview:nil];
+    [self setText:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -31,4 +39,294 @@
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+- (IBAction)button:(id)sender {
+    /*扫描二维码部分：
+    导入ZBarSDK文件并引入一下框架
+    AVFoundation.framework
+    CoreMedia.framework
+    CoreVideo.framework
+    QuartzCore.framework
+    libiconv.dylib
+    引入头文件#import “ZBarSDK.h” 即可使用
+    当找到条形码时，会执行代理方法
+    
+    - (void) imagePickerController: (UIImagePickerController*) reader didFinishPickingMediaWithInfo: (NSDictionary*) info
+    
+    最后读取并显示了条形码的图片和内容。*/
+    
+    /* 旧方法
+    ZBarReaderViewController *reader = [ZBarReaderViewController new];
+    reader.readerDelegate = self;
+    reader.supportedOrientationsMask = ZBarOrientationMaskAll;
+    
+    ZBarImageScanner *scanner = reader.scanner;
+    
+    [scanner setSymbology: ZBAR_I25
+                   config: ZBAR_CFG_ENABLE
+                       to: 0];
+    
+    [self presentModalViewController: reader
+                            animated: YES];
+    [reader release];*/
+    ZBarReaderViewController *reader = [ZBarReaderViewController new];
+    
+    reader.readerDelegate = self;
+    
+    //非全屏
+    
+    reader.wantsFullScreenLayout = NO;
+    
+    //隐藏底部控制按钮
+    
+    reader.showsZBarControls = NO;
+    
+    //设置自己定义的界面
+    
+    [self setOverlayPickerView:reader];
+    
+    ZBarImageScanner *scanner = reader.scanner;
+    
+    [scanner setSymbology: ZBAR_I25
+     
+                   config: ZBAR_CFG_ENABLE
+     
+                       to: 0];
+    
+    [self presentModalViewController: reader
+     
+                            animated: YES];
+    
+    [reader release];
+    
+}
+
+- (void)setOverlayPickerView:(ZBarReaderViewController *)reader
+
+{
+    
+    //清除原有控件
+    
+    for (UIView *temp in [reader.view subviews]) {
+        
+        for (UIButton *button in [temp subviews]) {
+            
+            if ([button isKindOfClass:[UIButton class]]) {
+                
+                [button removeFromSuperview];
+                
+            }
+            
+        }
+        
+        for (UIToolbar *toolbar in [temp subviews]) {
+            
+            if ([toolbar isKindOfClass:[UIToolbar class]]) {
+                
+                [toolbar setHidden:YES];
+                
+                [toolbar removeFromSuperview];
+                
+            }
+            
+        }
+        
+    }
+    
+    //画中间的基准线
+    
+    UIView* line = [[UIView alloc] initWithFrame:CGRectMake(40, 220, 240, 1)];
+    
+    line.backgroundColor = [UIColor redColor];
+    
+    [reader.view addSubview:line];
+    
+    [line release];
+    
+    //最上部view
+    
+    UIView* upView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 320, 80)];
+    
+    upView.alpha = 0.3;
+    
+    upView.backgroundColor = [UIColor blackColor];
+    
+    [reader.view addSubview:upView];
+    
+    //用于说明的label
+    
+    UILabel * labIntroudction= [[UILabel alloc] init];
+    
+    labIntroudction.backgroundColor = [UIColor clearColor];
+    
+    labIntroudction.frame=CGRectMake(15, 20, 290, 50);
+    
+    labIntroudction.numberOfLines=2;
+    
+    labIntroudction.textColor=[UIColor whiteColor];
+    
+    labIntroudction.text=@"将二维码图像置于矩形方框内，离手机摄像头10CM左右，系统会自动识别。";
+    
+    [upView addSubview:labIntroudction];
+    
+    [labIntroudction release];
+    
+    [upView release];
+    
+    //左侧的view
+    
+    UIView *leftView = [[UIView alloc] initWithFrame:CGRectMake(0, 80, 20, 280)];
+    
+    leftView.alpha = 0.3;
+    
+    leftView.backgroundColor = [UIColor blackColor];
+    
+    [reader.view addSubview:leftView];
+    
+    [leftView release];
+    
+    //右侧的view
+    
+    UIView *rightView = [[UIView alloc] initWithFrame:CGRectMake(300, 80, 20, 280)];
+    
+    rightView.alpha = 0.3;
+    
+    rightView.backgroundColor = [UIColor blackColor];
+    
+    [reader.view addSubview:rightView];
+    
+    [rightView release];
+    
+    //底部view
+    
+    UIView * downView = [[UIView alloc] initWithFrame:CGRectMake(0, 360, 320, 120)];
+    
+    downView.alpha = 0.3;
+    
+    downView.backgroundColor = [UIColor blackColor];
+    
+    [reader.view addSubview:downView];
+    
+    [downView release];
+    
+    //用于取消操作的button
+    
+    UIButton *cancelButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    
+    cancelButton.alpha = 0.4;
+    
+    [cancelButton setFrame:CGRectMake(20, 390, 280, 40)];
+    
+    [cancelButton setTitle:@"取消" forState:UIControlStateNormal];
+    
+    [cancelButton.titleLabel setFont:[UIFont boldSystemFontOfSize:20]];
+    
+    [cancelButton addTarget:self action:@selector(dismissOverlayView:)forControlEvents:UIControlEventTouchUpInside];
+    
+    [reader.view addSubview:cancelButton];  
+    
+}  
+
+//取消button方法  
+
+- (void)dismissOverlayView:(id)sender{   
+    
+    [self dismissModalViewControllerAnimated: YES];  
+    
+}
+
+- (IBAction)button2:(id)sender {
+    /*字符转二维码
+     导入 libqrencode文件
+     引入头文件#import "QRCodeGenerator.h" 即可使用
+     */
+	imageview.image = [QRCodeGenerator qrImageForString:text.text imageSize:imageview.bounds.size.width];
+    
+}
+
+- (IBAction)Responder:(id)sender {
+    //键盘释放
+    [text resignFirstResponder];
+
+}
+
+- (void) imagePickerController: (UIImagePickerController*) reader
+ didFinishPickingMediaWithInfo: (NSDictionary*) info
+{
+    id<NSFastEnumeration> results =
+    [info objectForKey: ZBarReaderControllerResults];
+    ZBarSymbol *symbol = nil;
+    for(symbol in results)
+        break;
+    
+    imageview.image =
+    [info objectForKey: UIImagePickerControllerOriginalImage];
+    
+    [reader dismissModalViewControllerAnimated: YES];
+    
+    //判断是否包含 头'http:'
+    NSString *regex = @"http+:[^\\s]*";
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",regex];
+    
+    //判断是否包含 头'ssid:'
+    NSString *ssid = @"ssid+:[^\\s]*";;
+    NSPredicate *ssidPre = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",ssid];
+    
+    label.text =  symbol.data ;
+    
+    if ([predicate evaluateWithObject:label.text]) {
+        
+        UIAlertView * alert = [[UIAlertView alloc]initWithTitle:nil
+                                                        message:@"It will use the browser to this URL。"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"Close"
+                                              otherButtonTitles:@"Ok", nil];
+        alert.delegate = self;
+        alert.tag=1;
+        [alert show];
+        [alert release];
+        
+        
+        
+    }
+    else if([ssidPre evaluateWithObject:label.text]){
+
+        NSArray *arr = [label.text componentsSeparatedByString:@";"];
+        
+        NSArray * arrInfoHead = [[arr objectAtIndex:0] componentsSeparatedByString:@":"];
+        
+        NSArray * arrInfoFoot = [[arr objectAtIndex:1] componentsSeparatedByString:@":"];
+        
+        
+        label.text=
+        [NSString stringWithFormat:@"ssid: %@ \n password:%@",
+         [arrInfoHead objectAtIndex:1],[arrInfoFoot objectAtIndex:1]];
+        
+        
+        UIAlertView * alert = [[UIAlertView alloc]initWithTitle:label.text
+                                                        message:@"The password is copied to the clipboard , it will be redirected to the network settings interface"
+                                                       delegate:nil
+                                              cancelButtonTitle:@"Close"
+                                              otherButtonTitles:@"Ok", nil];
+        
+        
+        alert.delegate = self;
+        alert.tag=2;
+        [alert show];
+        [alert release];
+        
+        UIPasteboard *pasteboard=[UIPasteboard generalPasteboard];
+        //        然后，可以使用如下代码来把一个字符串放置到剪贴板上：
+        pasteboard.string = [arrInfoFoot objectAtIndex:1]; 
+        
+        
+    }
+}
+
+
+- (void)dealloc {
+    [label release];
+    [imageview release];
+    [text release];
+    [super dealloc];
+}
 @end
